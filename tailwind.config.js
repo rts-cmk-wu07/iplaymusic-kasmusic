bd70d60094807903834b82c322e6603a640f1509
Kim <howtoad123@gmail.com>
2022-09-09 10:20:53 +0200

    set up of colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ff1168",
+        secondary: "#341931",
+        additional: "#111625",
+        gradientStart: "#ee0979",
+        gradientEnd: "#ff6a00",
+        darkBlue: "115793",
+        blue: "00a1cb",
+        cyan: "0abebe",
+        darkGreen: "3a7634",
+        green: "5eb11c",
+        yellow: "f2bc06",
+        orange: "f18d05",
+        red: "e54028",
+      },
+    },
   },
   plugins: [],
 };
