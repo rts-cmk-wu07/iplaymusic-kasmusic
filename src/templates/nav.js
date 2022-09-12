cbf6b365634559e10f868438f67aa8be112fcb1e
Alexander-Gronninger <alexander.gronninger@gmail.com>
2022-09-12 11:41:00 +0200

    add gradient to 1/5 icons

--- src/templates/nav.js
+++ src/templates/nav.js
@@ -11,32 +11,33 @@ const Nav = () => {
     <>
       <nav className="flex place-content-around items-center py-2 fixed bottom-0 w-full max-w-[425px]">
         <AlbumsOutline
-          color={"#00000"}
+          cssClasses="GRADIENTTEST"
+          color={"#000000"}
           title={"album"}
           height="25px"
           width="25px"
         />
         <MusicalNotesOutline
-          color={"#00000"}
+          color={"#000000"}
           title={"playlist"}
           height="25px"
           width="25px"
         />
         <WifiOutline
-          cssClasses="bg-black p-1 rounded-full"
+          cssClasses="p-1 rounded-full bg-gradient-to-r from-gradientStart to-gradientEnd"
           color={"#ffffff"}
           title={"featured"}
           height="45px"
           width="45px"
         />
         <ContrastOutline
-          color={"#00000"}
+          color={"#000000"}
           title={"light - dark"}
           height="25px"
           width="25px"
         />
         <FolderOpenOutline
-          color={"#00000"}
+          color={"#000000"}
           title={"categories"}
           height="25px"
           width="25px"
