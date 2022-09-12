62e033d1f155a0fc09071af30aef8fe3e88d21f7
Kim <howtoad123@gmail.com>
2022-09-12 10:54:20 +0200

    Test of gradient for heading component

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,9 +1,10 @@
 const Heading = ({ children, headingLevel, css }) => {
+  const gradientRules = "bg-gradient-to-r from-gradient-start to-gradient-end";
   const validHeadingLevels = ["h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8"];
 
   const safeHeading = headingLevel ? headingLevel.toLowerCase() : "";
   const Heading = validHeadingLevels.includes(safeHeading) ? safeHeading : "p";
-  return <Heading className={css}>{children}</Heading>;
+  return <Heading className={css + " " + gradientRules}>{children}</Heading>;
 };
 
 export default Heading;
