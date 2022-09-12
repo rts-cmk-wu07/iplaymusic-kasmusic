965d66ff03026db83f640c31319176c7b7a71b32
Alexander-Gronninger <alexander.gronninger@gmail.com>
2022-09-12 09:04:31 +0200

    create nav component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Nav from "./module/nav";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/module/nav.js
@@ -0,0 +1,15 @@
+const Nav = () => {
+  return (
+    <>
+      <section className="flex">
+        <ion-icon name="disc-outline"></ion-icon>
+        <ion-icon name="disc-outline"></ion-icon>
+        <ion-icon name="disc-outline"></ion-icon>
+        <ion-icon name="disc-outline"></ion-icon>
+        <ion-icon name="list-outline"></ion-icon>
+      </section>
+    </>
+  );
+};
+
+export default Nav;
