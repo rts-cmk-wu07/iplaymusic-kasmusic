a27f3ca1595b35f605fb20a39c44aa7048bfbe83
Alexander-Gronninger <alexander.gronninger@gmail.com>
2022-09-12 09:50:30 +0200

    nav done, without any functionality
    - add icons
    - sized icons
    - position nav to sticky bottom of screen
    - make featured icon bigger and have rounded background

--- src/templates/nav.js
+++ src/templates/nav.js
@@ -1,13 +1,47 @@
+import {
+  AlbumsOutline,
+  ContrastOutline,
+  FolderOpenOutline,
+  MusicalNotesOutline,
+  WifiOutline,
+} from "react-ionicons";
+
 const Nav = () => {
   return (
     <>
-      <section className="flex">
-        <ion-icon name="disc-outline"></ion-icon>
-        <ion-icon name="disc-outline"></ion-icon>
-        <ion-icon name="disc-outline"></ion-icon>
-        <ion-icon name="disc-outline"></ion-icon>
-        <ion-icon name="list-outline"></ion-icon>
-      </section>
+      <nav className="flex place-content-around items-center py-2 fixed bottom-0 w-full max-w-[425px]">
+        <AlbumsOutline
+          color={"#00000"}
+          title={"album"}
+          height="25px"
+          width="25px"
+        />
+        <MusicalNotesOutline
+          color={"#00000"}
+          title={"playlist"}
+          height="25px"
+          width="25px"
+        />
+        <WifiOutline
+          cssClasses="bg-black p-1 rounded-full"
+          color={"#ffffff"}
+          title={"featured"}
+          height="45px"
+          width="45px"
+        />
+        <ContrastOutline
+          color={"#00000"}
+          title={"light - dark"}
+          height="25px"
+          width="25px"
+        />
+        <FolderOpenOutline
+          color={"#00000"}
+          title={"categories"}
+          height="25px"
+          width="25px"
+        />
+      </nav>
     </>
   );
 };
