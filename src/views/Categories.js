49d5d2d04a16ed1036d283df96df29dbbe2a948f
Alexander-Gronninger <alexander.gronninger@gmail.com>
2022-09-15 09:37:24 +0200

    add heading

---
+++ src/views/Categories.js
@@ -0,0 +1,13 @@
+import Heading from "../components/Heading";
+
+const Categories = () => {
+  return (
+    <>
+      <section>
+        <Heading headingLevel="h1">Categories</Heading>
+      </section>
+    </>
+  );
+};
+
+export default Categories;
