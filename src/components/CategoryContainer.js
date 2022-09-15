3f4ab55f587b8b7f5f27ddfd9bb644e1b38971c4
Alexander-Gronninger <alexander.gronninger@gmail.com>
2022-09-15 11:17:05 +0200

    animate attempt with useState for animate prop

--- src/components/CategoryContainer.js
+++ src/components/CategoryContainer.js
@@ -1,19 +1,44 @@
 import { ChevronDownOutline } from "react-ionicons";
 import Category from "./Category";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 const CategoryContainer = ({ color, children }) => {
   const tempSubCategories = ["1", "2", "3", "4", "5", "6"];
+
+  const [showHide, setShowHide] = useState(false);
+  console.log(showHide);
   return (
     <>
       <section
+        onClick={() => {
+          setShowHide(!showHide);
+        }}
         className={
           "p-[25px] rounded-xl flex place-content-between my-4 " + color
         }
       >
         {children}
-        <ChevronDownOutline color={"#ffffff"} height="25px" width="25px" />
+        <motion.div
+          variants={({ open: { rotate: 180 } }, { close: { rotate: 0 } })}
+          initial="hidden"
+          animate={showHide ? "open" : "close"}
+          transition={{
+            duration: 0.5,
+          }}
+        >
+          <ChevronDownOutline color={"#ffffff"} height="25px" width="25px" />
+        </motion.div>
       </section>
-      <section className="mx-">
+      <motion.section
+        className="mx-6"
+        variants={({ show: { scale: 100 } }, { hidden: { scale: 0 } })}
+        initial="hidden"
+        animate={showHide ? "show" : "hidden"}
+        transition={{
+          duration: 0.5,
+        }}
+      >
         {tempSubCategories.map((category) => {
           return (
             <Category>
@@ -21,7 +46,7 @@ const CategoryContainer = ({ color, children }) => {
             </Category>
           );
         })}
-      </section>
+      </motion.section>
     </>
   );
 };
