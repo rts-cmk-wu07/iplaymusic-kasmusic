90f54454d870de77315db09cd703de4ab7b7dbb6
Alexander-Gronninger <alexander.gronninger@gmail.com>
2022-09-16 10:41:55 +0200

    setup of player, missing gradient on icons

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Featured from "./views/Featured";
 import { Routes, Route } from "react-router-dom";
 import Album from "./views/Album";
 import Categories from "./views/Categories";
+import Player from "./views/Player";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <TopMenu />
+      <Player />
       <Routes>
         <Route path="/" element={<Featured />} />
         <Route path="/albums" element={<Albums />} />
